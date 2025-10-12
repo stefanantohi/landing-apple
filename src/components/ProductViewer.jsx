92719f12ee7c90b43eac3d6f7ef917c532078b1c
Stefan <mail@stefanantohi.com>
2025-10-12 21:30:51 +0300

    add 3D models for MacBook variants and update ProductViewer

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -2,6 +2,8 @@ import useMacBookStore from "../store";
 import clsx from "clsx";
 import { Canvas } from "@react-three/fiber";
 import { Box, OrbitControls } from "@react-three/drei";
+import { MackbookModel } from "./models/Macbook";
+import MackbookModel14 from "./models/MackbookModel14";
 
 export default function ProductViewer() {
   const { color, scale, setColor, setScale } = useMacBookStore();
@@ -58,11 +60,8 @@ export default function ProductViewer() {
           id="canvas"
           camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}
         >
-          <Box
-            position={[0, 0, 0]}
-            scale={10 * scale}
-            material-color={color}
-          ></Box>
+          <ambientLight intensity={1} />
+          <MackbookModel14 scale={0.06} position={[-1, 0, 0]} />
           <OrbitControls enableZoom={false} />
         </Canvas>
       </section>
